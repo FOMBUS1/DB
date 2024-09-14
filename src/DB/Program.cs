c7c0f00e9c50c3c1a0212046447ca5c7e828fd33
FOMBUS1 <52848343+FOMBUS1@users.noreply.github.com>
2024-09-14 17:12:34 +0500

    Update Program.cs

--- src/DB/Program.cs
+++ src/DB/Program.cs
@@ -6,6 +6,30 @@ using System.Windows.Forms;
 
 namespace DB
 {
+    static class DataStorage
+    {
+        public static DataSet1 CreateDataset(string path = "DB.xml")
+        {
+            DataSet1 ds = new DataSet1();
+            ds.ReadXml(path);
+
+            return ds;
+        }
+        public static void CreateAppointment(DataTable appointments, int doctorId, string patientName)
+        {
+            DataRow row = appointments.NewRow();
+            row.ItemArray = new object[] { null, doctorId, patientName };
+            appointments.Rows.Add(row);
+        }
+
+        public static void CreateDoctor(DataTable doctors, string Fullname, DateTime birthday, int departmentId, int jobTitleId, int passportSeries, int passportNumber)
+        {
+            DataRow row = doctors.NewRow();
+            row.ItemArray = new object[] { null, Fullname, birthday, departmentId, jobTitleId, passportSeries, passportNumber };
+            doctors.Rows.Add(row);
+        }
+    }
+    
     internal static class Program
     {
         /// <summary>
